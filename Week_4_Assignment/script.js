a6b53180589b55be9bdaadb0bf234e2b3f5f00c8
RonaldBasil <ronn11223344@gmail.com>
2020-10-15 08:38:46 +0530

    Week 4 Assignment

--- Week_4_Assignment/script.js
+++ Week_4_Assignment/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (window) {
+
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -58,12 +58,10 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter === "j") {
-    console.log(byeSpeaker.speak(names[i]));
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
   } 
   else {
-    console.log(helloSpeaker.speak(names[i]));
+    helloSpeaker.speak(names[i]);
   }
 }
-
-})(window);
